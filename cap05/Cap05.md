36a7b1b44a72bd6fc6f78a62a239520145937eb8
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-30 13:45:33 -0300

    Capítulo 05 -  Construtores, palavra this, sobrecarga, encapsulamento

---
+++ cap05/Cap05.md
@@ -0,0 +1,16 @@
+# Construtores, palavra this, sobrecarga, encapsulamento
+
+49. Visão sobre o capítulo
+50. Material de apoio do capítulo
+51. Construtores
+52. Sobrecarga
+53. Sintaxe alternativa para inicializar valores
+54. Palavra this
+55. Encapsulamento
+56. Properties
+57. Auto Properties
+58. Ordem sugerida para implementação de membros de classe
+59. Modificadores de acesso
+60. Exercício de fixação
+61. Correção do exercício - PARTE 1
+62. Correção do exercício - PARTE 2
---
+++ cap05/exercicio01/Course/ContaBancaria.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+    class ContaBancaria
+    {
+
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numero, string titular)
+        {
+            this.Numero = numero;
+            this.Titular = titular;
+        }
+
+        public ContaBancaria(int numero, string titular, double saldo) : this(numero, titular)
+        {
+            this.Saldo = saldo;
+        }
+
+        public void Deposito(double quantia)
+        {
+            this.Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            this.Saldo -= quantia + 5.0;
+        }
+
+        public override string ToString() => "Conta "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+
+    }
+}
